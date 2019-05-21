d4811f9f3bb7caad08d444acecd81d00525c75e5
vagrant <you@example.com>
2019-05-21 20:53:56 +0000

    created function to look up people in the table by first or last name

---
+++ lookup_people.js
@@ -0,0 +1,39 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+	user: settings.user,
+	password: settings.password,
+	database: settings.database,
+	host: settings.hostname,
+	port: settings.port,
+	ssl: settings.ssl
+});
+
+client.connect(err => {
+	if (err) {
+		return console.error("Connection Error", err);
+	}
+	var name = process.argv.slice(2)[0];
+	client.query(
+		"SELECT * FROM famous_people WHERE first_name =$1 OR last_name =$1",
+		[name],
+		(err, res) => {
+			if (err) {
+				return console.error("error running query", err);
+			}
+			res.rows.forEach(row =>
+				console.log(
+					"- " +
+						row.first_name +
+						" " +
+						row.last_name +
+						", born " +
+						row.birthdate.toLocaleDateString()
+				)
+			);
+			// console.log(res.rows);
+			client.end();
+		}
+	);
+});
