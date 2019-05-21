4ad2c5e19a43002d8433d32c241d9e107a4eb397
vagrant <you@example.com>
2019-05-21 23:07:36 +0000

    convert query to knex

---
+++ search_people.js
@@ -0,0 +1,34 @@
+const pg = require("pg");
+var settings = require("./settings");
+// console.log('settings:', settings);
+var knex = require("knex")({
+	client: "pg",
+	connection: {
+		host: settings.hostname,
+		user: settings.user,
+		password: settings.password,
+		database: settings.database
+	}
+});
+var name = process.argv[2];
+knex.select("first_name", "last_name", "birthdate")
+	.where({ first_name: name })
+	.orWhere({ last_name: name })
+	.from("famous_people")
+	.asCallback(function(err, rows) {
+		if (err) return console.error(err);
+		rows.forEach((row, index) =>
+			console.log(
+				"- " +
+					(index + 1) +
+					": " +
+					row.first_name +
+					" " +
+					row.last_name +
+					", born " +
+					row.birthdate.toLocaleDateString()
+			)
+		);
+
+		knex.destroy();
+	});
